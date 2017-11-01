7ea4c17dabf5e9ee57b3a30fb4110de3cd978383
Matthias Arndt <marndt@asmsoftware.de>
2017-11-01 11:21:45 +0100

    - comment test program

--- src/testapp/main.c
+++ src/testapp/main.c
@@ -11,76 +11,75 @@
 #include "osbind.h"
 #include "conio.h"
 
-static void wait_for_key(void)
+static void wait_for_key ( void )
 {
-    gotoxy(0,24);
+    /* prompt in revers video */
+	gotoxy ( 0,24 );
+	revers ( 1 );
+	textcolor ( 15 );
+	bgcolor ( 0 );
+	cputs ( ">> press any key <<" );
+	revers ( 0 );
 
-    revers(1);
-    textcolor(15);
-    bgcolor(0);
-    cputs(">> press any key <<");
-    revers(0);
+	while ( kbhit() == 0 )
+	{
+		;
+	}
 
-
-    while(kbhit() == 0)
-    {
-        ;
-    }
-    (void)cgetc();
-
-    return;
+	( void ) cgetc();
+	return;
 }
 
-int main(int argc, char **argv)
+int main ( int argc, char **argv )
 {
-    uint8_t col, x,y;
-    char *your_name;
+	uint8_t col, x,y;
+	char *your_name;
+	( void ) argc;
+	( void ) argv;
 
-    (void)argc;
-    (void)argv;
+    /* some stars with colors */
+	clrscr();
+	for ( y = 0; y < 3; y++ )
+	{
+		for ( x = 0; x < 40; x++ )
+		{
+			textcolor ( ( col++ % 15 )+1 );
+			bgcolor ( 0 );
+			cputcxy ( x,y,'*' );
+		}
+	}
 
-    clrscr();
+    /*  write color codes */
+	for ( col = 0; col < 16; col++ )
+	{
+		textcolor ( col );
+		gotoxy ( 1,5+col );
+		cputs ( "Color 0x" );
+		cputhex8 ( col );
+	}
 
-    for(y = 0; y < 3; y++)
-    {
-        for(x = 0; x < 40; x++)
-        {
-            textcolor((col++ % 15)+1);
-            bgcolor(0);
-            cputcxy(x,y,'*');
-        }
-    }
+    /* test line drawing: */
+    textcolor(3);
+    chlinexy(30,15,10);
+    chlinexy(30,20,10);
+    cvlinexy(30,16,4);
+    cvlinexy(39,16,4);
+	gotoxy ( 0,4 );
+	textcolor ( 14 );
+	chline ( 40 );
+	wait_for_key();
 
-    for(col = 0; col < 16; col++)
-    {
-        textcolor(col);
-        gotoxy(1,5+col);
-        cputs("Color 0x");
-        cputhex8(col);
-    }
+    /* test string input with cursor shown */
+	clrscr();
+	textcolor ( 15 );
+	cputsxy ( 0,0, "Enter your name:" );
+	cursor ( 1 );
+	gotoxy ( 0,1 );
+	your_name = cgets();
+	cursor ( 0 );
+	cputsxy ( 0,2, "Your name is " );
+	cputs ( your_name );
+	wait_for_key();
 
-    textcolor(9);
-    revers(1);
-    cputsxy(20,13,"TEST");
-    revers(0);
-
-    gotoxy(0,4);
-    textcolor(14);
-    chline(40);
-
-    wait_for_key();
-
-    clrscr();
-    textcolor(15);
-    cputsxy(0,0, "Enter your name:");
-    cursor(1);
-    gotoxy(0,1);
-    your_name = cgets();
-    cursor(0);
-    cputsxy(0,2, "Your name is ");
-    cputs(your_name);
-
-    wait_for_key();
-
-    return 0;
+	return 0;
 }
